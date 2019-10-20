1001c4e51646d07dfb4ec59f0390523994eb24a9
james ross <james_ross@outlook.fr>
2019-10-20 19:01:56 +0200

    first push

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include 'treatment.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Contact Form PHP</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+
+  <div class='backgroundImage'>
+    <div class="marginBox">
+      <div class='textBox'>
+          <h1>Chinese Restaurant</h1>
+        <p>Tell us what you think... We'd love to hear from you</p>
+      </div>
+    </div>
+    <div class="formDiv">
+      <form action="index.php" method="post">
+        <input type="text" class='form' name="name" id="name" placeholder="Your name" required>
+        <input type="email" class='form' name="email" id="email" placeholder="Your email" required>
+        <input type="tel" class='form' name="telephone" id="telephone" placeholder="Your cell number" required>
+        <textarea class='form border' name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+        <input type="submit" class='form submit' value="Send Message">
+      </form>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
---
+++ treatment.php
@@ -0,0 +1,21 @@
+<?php
+$userName = $_POST['name'];
+$userEmail = $_POST['email'];
+$userPhone = $_POST['telephone'];
+$userMessage = $_POST['message'];
+
+$emailFrom = 'james_ross@outlook.fr';
+
+$emailSubject = 'New Message From Visitor';
+$emailBody = 'Visitor Name: $userName.\n'.
+'Visitor Email: $userEmail.\n'.
+'Visitor Telephone: $userPhone.\n'. 
+'Message: $userMessage.\n'
+
+$emailTo = 'james_ross@outlook.fr'
+
+$headers = 'From: $emailFrom \r\n';
+$headers .= 'Reply-To: $userEmail \r\n';
+
+mail($emailTo,$emailSubject,$emailBody,$headers);
+header('Location: index.php');
